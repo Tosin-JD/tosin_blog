e10d4cb1eecaa6cb96dce7fdee074e5f57c0a70a
Tosin-JD <oluwatosinjosephdurodola@gmail.com>
2021-03-15 17:09:48 +0100

    full debug4

--- blog/urls.py
+++ blog/urls.py
@@ -13,8 +13,8 @@ app_name = 'blog'
 urlpatterns = [
     path('create/', DeleteBlog.as_view(), name='create'),
     path('', BlogList.as_view(), name='list'),
-    path('<slug>', BlogDetail.as_view(), name='detail'),
     path('<slug>/delete/', CreateBlog.as_view(), name='delete'),
     path('<slug>/update/', UpdateBlog.as_view(), name='update'),
+    path('<slug>', BlogDetail.as_view(), name='detail'),
 ]
 
