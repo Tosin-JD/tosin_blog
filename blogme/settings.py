8d626e285e3441585270717fdbf600de8c4d44c0
Tosin-JD <oluwatosinjosephdurodola@gmail.com>
2021-03-15 14:31:37 +0100

    More changes to settings

--- blogme/settings.py
+++ blogme/settings.py
@@ -137,3 +137,6 @@ STATFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
